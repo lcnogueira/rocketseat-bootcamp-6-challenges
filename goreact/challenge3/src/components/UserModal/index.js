dd1cf98c1abf8b172131d38d7a3525b06cd984a1
Luiz Claudio <luizcns18@gmail.com>
2019-05-06 12:37:07 -0300

    refactor: Turn modal into a hook component

--- goreact/challenge3/src/components/UserModal/index.js
+++ goreact/challenge3/src/components/UserModal/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Modal from 'react-modal';
 import PropTypes from 'prop-types';
 
@@ -13,75 +13,63 @@ import './style.css';
 
 Modal.setAppElement(document.getElementById('root'));
 
-class UserModal extends Component {
-  static propTypes = {
-    modal: PropTypes.shape({
-      show: PropTypes.bool,
-      coord: PropTypes.shape({
-        latitude: PropTypes.number,
-        longitude: PropTypes.number,
-      }),
-    }).isRequired,
-    hideModal: PropTypes.func.isRequired,
-    addUserRequest: PropTypes.func.isRequired,
-  };
+function UserModal({ hideModal, addUserRequest, modal }) {
+  const [user, setUser] = useState('');
 
-  state = {
-    user: '',
-  };
+  function closeModal() {
+    hideModal();
+    setUser('');
+  }
 
-  resetState = () => this.setState({ user: '' });
-
-  closeModal = () => {
-    this.props.hideModal();
-    this.resetState();
-  };
-
-  submitUser = (e) => {
+  function submitUser(e) {
     e.preventDefault();
-
-    const { user } = this.state;
 
     if (!user) {
       toast.error('Set a github username');
       return;
     }
 
-    const { latitude, longitude } = this.props.modal.coord;
+    const { latitude, longitude } = modal.coord;
 
-    this.props.addUserRequest({ user, coord: { latitude, longitude } });
-    this.resetState();
-  };
-
-  render() {
-    return (
-      <Modal
-        isOpen={this.props.modal.show}
-        onRequestClose={this.closeModal}
-        contentLabel="Add User Modal"
-        className="modal-container"
-        overlayClassName="modal-overlay"
-      >
-        <h2>Add a github user</h2>
-        <form onSubmit={this.submitUser} className="modal-form">
-          <input
-            type="text"
-            placeholder="Github User"
-            onChange={e => this.setState({ user: e.target.value })}
-          />
-          <div className="modal-buttons">
-            <button type="button" onClick={this.closeModal} className="bg-danger">
-              Cancel
-            </button>
-            <button type="submit" className="bg-primary">
-              Send
-            </button>
-          </div>
-        </form>
-      </Modal>
-    );
+    addUserRequest({ user, coord: { latitude, longitude } });
+    setUser('');
   }
+
+  return (
+    <Modal
+      isOpen={modal.show}
+      onRequestClose={closeModal}
+      contentLabel="Add User Modal"
+      className="modal-container"
+      overlayClassName="modal-overlay"
+    >
+      <h2>Add a github user</h2>
+      <form onSubmit={submitUser} className="modal-form">
+        <input type="text" placeholder="Github User" onChange={e => setUser(e.target.value)} />
+        <div className="modal-buttons">
+          <button type="button" onClick={closeModal} className="bg-danger">
+            Cancel
+          </button>
+          <button type="submit" className="bg-primary">
+            Send
+          </button>
+        </div>
+      </form>
+    </Modal>
+  );
 }
+
+UserModal.propTypes = {
+  modal: PropTypes.shape({
+    show: PropTypes.bool,
+    coord: PropTypes.shape({
+      latitude: PropTypes.number,
+      longitude: PropTypes.number,
+    }),
+  }).isRequired,
+  hideModal: PropTypes.func.isRequired,
+  addUserRequest: PropTypes.func.isRequired,
+};
 
 const mapStToProps = ({ modal }) => ({ modal });
 
