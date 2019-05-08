49160d7c589186b4c717a2c1d076f27f26d60f4a
Luiz Claudio <luizcns18@gmail.com>
2019-05-08 17:36:52 -0300

    Add module2

--- gonative/challenge2/src/index.js
+++ gonative/challenge2/src/index.js
@@ -1,9 +1,32 @@
 import './config/ReactotronConfig';
+import AsyncStorage from '@react-native-community/async-storage';
 
-import React from 'react';
+import React, { Component } from 'react';
 
-import Main from './pages/Main';
+import createNavigator from './routes';
 
-const App = () => <Main />;
+export default class App extends Component {
+  state = {
+    userChecked: false,
+    userLogged: false,
+  };
 
-export default App;
+  async componentDidMount() {
+    const username = await AsyncStorage.getItem('@Githuber:username');
+
+    this.setState({
+      userChecked: true,
+      userLogged: !!username,
+    });
+  }
+
+  render() {
+    const { userChecked, userLogged } = this.state;
+
+    if (!userChecked) return null;
+
+    const Routes = createNavigator(userLogged);
+
+    return <Routes />;
+  }
+}
