fc5a653b9a6a45a07e2eb6582eba99dc98256ee0
Luiz Claudio <luizcns18@gmail.com>
2019-05-24 18:37:02 -0300

    feat: Internationalization

--- gonode/challenge4/app/Validators/BaseValidator.js
+++ gonode/challenge4/app/Validators/BaseValidator.js
@@ -1,9 +1,15 @@
 'use strict'
 
+const Antl = use('Antl')
+
 class BaseValidator {
   // Validate all the fields and send all the error messages. Not only the first wrong one.
   get validateAll () {
     return true
+  }
+
+  get messages () {
+    return Antl.list('validation')
   }
 }
 
