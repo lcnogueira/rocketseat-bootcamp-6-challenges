50928ade418d362c0fdb857d027e01602e07d009
Luiz Claudio <luizcns18@gmail.com>
2019-05-27 16:03:11 -0300

    Add global style

--- goreact/challenge4/src/App.js
+++ goreact/challenge4/src/App.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import GlobalStyle from './styles/global';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <Fragment>
+      <GlobalStyle />
+      <h1>Hello World</h1>
+    </Fragment>
+  );
 }
 
 export default App;
