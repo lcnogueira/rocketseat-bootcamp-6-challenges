dfb88a730952b37c8b10f819f29a4cd08c13fb14
Luiz Claudio <luizcns18@gmail.com>
2019-05-28 10:49:54 -0300

    feat: Load playlists on browse page

--- goreact/challenge4/src/pages/browse/index.js
+++ goreact/challenge4/src/pages/browse/index.js
@@ -1,48 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Creators as PlaylistsActions } from '../../store/ducks/playlists';
 
 import {
   Container, Title, List, Playlist,
 } from './styles';
 
-const Browse = () => (
-  <Container>
-    <Title>Browse</Title>
+class Browse extends Component {
+  static propTypes = {
+    getPlayListsRequest: PropTypes.func.isRequired,
+    playlists: PropTypes.shape({
+      data: PropTypes.arrayOf(
+        PropTypes.shape({
+          id: PropTypes.number,
+          title: PropTypes.string,
+          thumbnail: PropTypes.string,
+          description: PropTypes.string,
+        }),
+      ),
+    }).isRequired,
+  };
 
-    <List>
-      <Playlist to="/playlists/1">
-        <img
-          src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/Stargroves-album-cover.png?auto=format&q=60&fit=max&w=930"
-          alt="Playlist"
-        />
-        <strong>Old Rock</strong>
-        <p>Relax while programming by listening the best of the world rock</p>
-      </Playlist>
-      <Playlist to="/playlists/1">
-        <img
-          src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/Stargroves-album-cover.png?auto=format&q=60&fit=max&w=930"
-          alt="Playlist"
-        />
-        <strong>Old Rock</strong>
-        <p>Relax while programming by listening the best of the world rock</p>
-      </Playlist>
-      <Playlist to="/playlists/1">
-        <img
-          src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/Stargroves-album-cover.png?auto=format&q=60&fit=max&w=930"
-          alt="Playlist"
-        />
-        <strong>Old Rock</strong>
-        <p>Relax while programming by listening the best of the world rock</p>
-      </Playlist>
-      <Playlist to="/playlists/1">
-        <img
-          src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/Stargroves-album-cover.png?auto=format&q=60&fit=max&w=930"
-          alt="Playlist"
-        />
-        <strong>Old Rock</strong>
-        <p>Relax while programming by listening the best of the world rock</p>
-      </Playlist>
-    </List>
-  </Container>
-);
+  componentDidMount() {
+    const { getPlayListsRequest } = this.props;
+    getPlayListsRequest();
+  }
 
-export default Browse;
+  render() {
+    const { playlists } = this.props;
+    return (
+      <Container>
+        <Title>Browse</Title>
+
+        <List>
+          {playlists.data.map(playlist => (
+            <Playlist key={playlist.id} to={`/playlists/${playlist.id}`}>
+              <img src={playlist.thumbnail} alt={playlist.title} />
+              <strong>{playlist.title}</strong>
+              <p>{playlist.description}</p>
+            </Playlist>
+          ))}
+        </List>
+      </Container>
+    );
+  }
+}
+
+const mapStateToProps = ({ playlists }) => ({ playlists });
+
+const mapDispatchToProps = dispatch => bindActionCreators(PlaylistsActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Browse);
