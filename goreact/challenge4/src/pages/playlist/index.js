d29bfe8d49b5622b57753fc4b63f087c94f21c8b
Luiz Claudio <luizcns18@gmail.com>
2019-05-28 15:28:40 -0300

    feat: Add playlist detail behaviour

--- goreact/challenge4/src/pages/playlist/index.js
+++ goreact/challenge4/src/pages/playlist/index.js
@@ -1,103 +1,129 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Creators as PlaylistDetailsActions } from '../../store/ducks/playlistDetails';
 
 import { Container, Header, SongList } from './styles';
+
+import Loading from '../../components/Loading';
 
 import ClockIcon from '../../assets/images/clock.svg';
 import PlusIcon from '../../assets/images/plus.svg';
 
-const Playlist = () => (
-  <Container>
-    <Header>
-      <img
-        src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/Stargroves-album-cover.png?auto=format&q=60&fit=max&w=930"
-        alt="Playlist"
-      />
-      <div>
-        <span>PLAYLIST</span>
-        <h1>Rock Forever</h1>
-        <p>13 songs</p>
+class Playlist extends Component {
+  static propTypes = {
+    getPlaylistDetailsRequest: PropTypes.func.isRequired,
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        id: PropTypes.number,
+      }),
+    }).isRequired,
+    playlistDetails: PropTypes.shape({
+      data: PropTypes.shape({
+        thumbnail: PropTypes.string,
+        title: PropTypes.string,
+        description: PropTypes.string,
+        songs: PropTypes.arrayOf(
+          PropTypes.shape({
+            id: PropTypes.number,
+            title: PropTypes.string,
+            author: PropTypes.string,
+            album: PropTypes.string,
+          }),
+        ),
+      }),
+      loading: PropTypes.bool,
+    }).isRequired,
+  };
 
-        <button type="button">PLAY</button>
-      </div>
-    </Header>
+  componentDidMount() {
+    this.loadPlaylistDetails();
+  }
 
-    <SongList cellPadding={0} cellSpacing={0}>
-      <thead>
-        <th />
-        <th>Title</th>
-        <th>Artist</th>
-        <th>Album</th>
-        <th>
-          <img src={ClockIcon} alt="Time" />{' '}
-        </th>
-      </thead>
-      <tbody>
-        <tr>
-          <td>
-            <img src={PlusIcon} alt="Add" />
-          </td>
-          <td>Papercut</td>
-          <td>Linkin Park</td>
-          <td>Hybrid Theory</td>
-          <td>3:26</td>
-        </tr>
-        <tr>
-          <td>
-            <img src={PlusIcon} alt="Add" />
-          </td>
-          <td>Papercut</td>
-          <td>Linkin Park</td>
-          <td>Hybrid Theory</td>
-          <td>3:26</td>
-        </tr>
-        <tr>
-          <td>
-            <img src={PlusIcon} alt="Add" />
-          </td>
-          <td>Papercut</td>
-          <td>Linkin Park</td>
-          <td>Hybrid Theory</td>
-          <td>3:26</td>
-        </tr>
-        <tr>
-          <td>
-            <img src={PlusIcon} alt="Add" />
-          </td>
-          <td>Papercut</td>
-          <td>Linkin Park</td>
-          <td>Hybrid Theory</td>
-          <td>3:26</td>
-        </tr>
-        <tr>
-          <td>
-            <img src={PlusIcon} alt="Add" />
-          </td>
-          <td>Papercut</td>
-          <td>Linkin Park</td>
-          <td>Hybrid Theory</td>
-          <td>3:26</td>
-        </tr>
-        <tr>
-          <td>
-            <img src={PlusIcon} alt="Add" />
-          </td>
-          <td>Papercut</td>
-          <td>Linkin Park</td>
-          <td>Hybrid Theory</td>
-          <td>3:26</td>
-        </tr>
-        <tr>
-          <td>
-            <img src={PlusIcon} alt="Add" />
-          </td>
-          <td>Papercut</td>
-          <td>Linkin Park</td>
-          <td>Hybrid Theory</td>
-          <td>3:26</td>
-        </tr>
-      </tbody>
-    </SongList>
-  </Container>
-);
+  componentDidUpdate(prevProps) {
+    const { match } = this.props;
+    if (prevProps.match.params.id !== match.params.id) {
+      this.loadPlaylistDetails();
+    }
+  }
 
-export default Playlist;
+  loadPlaylistDetails = () => {
+    const { getPlaylistDetailsRequest, match } = this.props;
+    const { id } = match.params;
+
+    getPlaylistDetailsRequest(id);
+  };
+
+  renderDetails = () => {
+    const {
+      playlistDetails: { data: playlist },
+    } = this.props;
+    return (
+      <Container>
+        <Header>
+          <img src={playlist.thumbnail} alt={playlist.title} />
+          <div>
+            <span>PLAYLIST</span>
+            <h1>{playlist.title}</h1>
+            {!!playlist.songs && <p>{playlist.songs.length}</p>}
+
+            <button type="button">PLAY</button>
+          </div>
+        </Header>
+
+        <SongList cellPadding={0} cellSpacing={0}>
+          <thead>
+            <th />
+            <th>Title</th>
+            <th>Artist</th>
+            <th>Album</th>
+            <th>
+              <img src={ClockIcon} alt="Time" />{' '}
+            </th>
+          </thead>
+          <tbody>
+            {!playlist.songs ? (
+              <tr>
+                <td colSpan={5}>No music found</td>
+              </tr>
+            ) : (
+              playlist.songs.map(song => (
+                <tr key={song.id}>
+                  <td>
+                    <img src={PlusIcon} alt="Add" />
+                  </td>
+                  <td>{song.title}</td>
+                  <td>{song.author}</td>
+                  <td>{song.album}</td>
+                  <td>3:26</td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </SongList>
+      </Container>
+    );
+  };
+
+  render() {
+    const { playlistDetails } = this.props;
+    return playlistDetails.loading ? (
+      <Container loading>
+        <Loading />
+      </Container>
+    ) : (
+      this.renderDetails()
+    );
+  }
+}
+
+const mapStateToProps = ({ playlistDetails }) => ({ playlistDetails });
+
+const mapDispatchToProps = dispatch => bindActionCreators(PlaylistDetailsActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Playlist);
---
+++ goreact/challenge4/src/store/ducks/playlistDetails.js
@@ -0,0 +1,41 @@
+/**
+ * Action Types
+ */
+export const Types = {
+  GET_REQUEST: 'playlistDetails/GET_REQUEST',
+  GET_SUCCESS: 'playlistDetails/GET_SUCCESS',
+};
+
+const INITIAL_STATE = {
+  data: {},
+  loading: false,
+};
+
+/**
+ * Reducers
+ */
+export default function playlistDetails(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case Types.GET_REQUEST:
+      return { ...state, loading: true };
+    case Types.GET_SUCCESS:
+      return { ...state, loading: false, data: action.payload.data };
+    default:
+      return state;
+  }
+}
+
+/**
+ * Action Creators
+ */
+export const Creators = {
+  getPlaylistDetailsRequest: id => ({
+    type: Types.GET_REQUEST,
+    payload: { id },
+  }),
+
+  getPlaylistDetailsSuccess: data => ({
+    type: Types.GET_SUCCESS,
+    payload: { data },
+  }),
+};
